6561464fac1073ebf862a52e76c9876915363f59
Reid Madock <reidmadock@gmail.com>
2023-02-21 12:09:30 -0600

    Clearing all buttons between questions finally works, now need the rest of requirements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function quizTimer() {
 
 function beginQuiz() {
     quizTimer();
-    var questionNum = 0;
+    //var questionNum = 0;
     startScreen.style.visibility = "collapse";
     quizScreen.style.visibility = "visible";
     loadQuestion(0);
@@ -27,20 +27,8 @@ function loadQuestion(num) {
     var questionText = document.getElementById("question-prompt");
     var answerButtons = document.getElementById("answer-options");
     var correctAnswer = answerKey[num];
-    //console.log("answer key", correctAnswer);
     questionText.textContent = quesAndAns[num][0];
 
-    // if (answerButtons.hasChildNodes) {
-    //     // console.log(answerButtons.childNodes);
-    //     answerButtons.childNodes.forEach(element => element.remove());
-    // }
-    // if (answerButtons.hasChildNodes.length > 0) {  
-    //     while (answerButtons.hasChildNodes) {
-    //         answerButtons.removeChild(answerButtons.firstChild);
-    //     }    
-    // }
-
-    // console.log("Amount of Answers",quesAndAns[num].length)
     for (var i = 1; i < quesAndAns[num].length; i++) {
         var button = document.createElement('button');
         button.setAttribute('class', 'option');
@@ -48,50 +36,45 @@ function loadQuestion(num) {
         //button.setAttribute('text', quesAndAns[num][i]);
         button.textContent = quesAndAns[num][i];
         answerButtons.appendChild(button);
+
         button.addEventListener("click", function(event) {
-            //checkAnswer();
-            // console.log(event.target);
-            // console.log(event.target.dataset.number);
             if (event.target.dataset.number == correctAnswer) {
                 console.log("correct!");
                 highscore++;
             } else {
                 console.log("Incorrect :(");
             }
-            // clearWindow();
+            clearWindow();
             if (num < answerKey.length) {
                 // clearWindow()
                 loadQuestion(++num);
             }
         })
     }
-
-    // document.addEventListener("option", function  (event) {
-    //     console.log("fired");
-    // })
 }
 
 function clearWindow() {
     var questionText = document.getElementById("question-prompt");
+    console.log("Clearing window...");
     var answerButtons = document.getElementById("answer-options");
     questionText.textContent = ""
-    var allButtons = document.getElementsByClassName("option")
-    console.log("hopefully this is all the buttons: ",allButtons);
-    for(var i = 0; i < allButtons.length; i++) {
-        //allButtons[i].removeEventListener();
-        allButtons[i].remove();
+    // var allButtons = document.getElementsByClassName("option")
+    // console.log("hopefully this is all the buttons: ", allButtons);
+    // for(var i = 0; i < allButtons.length; i++) {
+    //     allButtons[i].remove();
+    // }
+    // console.log(answerButtons.childNodes);
+    // for (var i = 0; i < answerButtons.childNodes.length; i++) {
+    //     console.log("Removing first child...")
+    //     answerButtons.firstChild.remove();
+    // }
+    if (answerButtons.childNodes.length > 0) {
+        console.log("Removing first child recursively...")
+        answerButtons.firstChild.remove();
+        clearWindow();
     }
     
 }
-
-// function checkAnswer() {
-//     console.log("fired");
-// }
-
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-
-// })
 
 quizStartButton.addEventListener("click", beginQuiz);
 /* 2D array, [][] -> [question #][question data] where N_{0} is question data and N_{1,..,M} are options.
